8419c612e07ce1fd5001c229821bfad4936d3df3
TalaNetwork GmbH <125084137+TalaDev@users.noreply.github.com>
2023-04-06 01:56:51 +0330

    Add files via upload

---
+++ cencel.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html dir="rtl" lang="fa">
+ <head>
+<meta charset="utf-8">
+<title>پرداخت ناموفق</title>
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<style type="text/css">
+ body
+{
+background:#f2f2f2;
+}
+.payment
+{
+border:1px solid #f2f2f2;
+height:280px;
+border-radius:20px;
+background:#fff;
+}
+.payment_header
+{
+background:rgba(255,102,0,1);
+padding:20px;
+border-radius:20px 20px 0px 0px;
+}
+.check
+{
+margin:0px auto;
+width:50px;
+height:50px;
+border-radius:100%;
+background:#fff;
+text-align:center;
+}
+.check i
+{
+vertical-align:middle;
+line-height:50px;
+font-size:30px;
+}
+.content 
+{
+text-align:center;
+}
+.content  h1
+{
+font-size:25px;
+padding-top:25px;
+}
+.content a
+{
+width:200px;
+height:35px;
+color:#fff;
+border-radius:30px;
+padding:5px 10px;
+background:rgba(255,102,0,1);
+transition:all ease-in-out 0.3s;
+}
+.content a:hover
+{
+text-decoration:none;
+background:#000;
+}
+.video_ad
+{
+display: inline-block;
+width: 48%;
+margin-top: 20px;
+margin-left: 1%;
+}
+</style>
+</head>
+<body>
+<div class="container">
+<div class="row">
+  <div class="col-md-6 mx-auto mt-5">
+ <div class="payment">
+<div class="payment_header">
+<div class="check"><i class="fa fa-check" aria-hidden="true"></i></div>
+</div>
+<div class="content">
+<h1>پرداخت نا موفق !</h1>
+<p>پرداخت شما تایید نشد در صورت کسر وجه مبلغ ان طی 72 ساعت به حساب شما به صورت خودکار برگشت داده میشود</p>
+<a href="tg://resolve?domain=nimvip_bot"
+echo "{$_GET["id"]}";
+?>">برگشت به ربات</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
